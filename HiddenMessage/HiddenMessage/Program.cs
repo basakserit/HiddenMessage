bd94c529fc2f666b764c6071cc24ed7ef004244c
Basak Serit <basak.serit@softensity.com>
2020-02-12 10:46:07 +0300

    first commit

---
+++ HiddenMessage/HiddenMessage/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace HiddenMessage
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            bool showMenu = true;
+            while (showMenu)
+            {
+                showMenu = ShowMenu();
+            }
+        }
+
+        static bool ShowMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("## Hidden Words ###");
+            Console.WriteLine("1 - Encryption");
+            Console.WriteLine("2 - Decryption");
+            Console.WriteLine("3 - EXIT");
+
+            Console.WriteLine("Please select > ");
+
+            var selection = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(selection) || selection.Equals("3"))
+                return false;
+
+            switch (selection)
+            {
+                case "1":
+                    EncryptText();
+                    return false;
+                case "2":
+                    DecryptText();
+                    return false;
+                default:
+                    return true;
+            }
+        }
+
+        static void EncryptText()
+        {
+            WordOperations wo = new WordOperations();
+
+            Console.WriteLine("Please type a message to encrypt > ");
+            var text = Console.ReadLine();
+
+            wo.WordWitoutEncrytion = text;
+
+            Console.WriteLine("Encrypted message > " + wo.EncryptWord());
+        }
+
+        static void DecryptText()
+        {
+            WordOperations wo = new WordOperations();
+
+            Console.WriteLine("Please type a message to decrypt > ");
+            var text = Console.ReadLine();
+
+            wo.WordWitoutEncrytion = text;
+
+            Console.WriteLine("Decrypted message > " + wo.DecryptWord());
+        }
+
+    }
+}
---
+++ HiddenMessage/HiddenMessage/WordOperations.cs
@@ -0,0 +1,94 @@
+﻿using System;
+namespace HiddenMessage
+{
+    public class WordOperations
+    {
+        string wordWithoutEncryption, encryptedWord, decryptedWord;
+
+        public WordOperations()
+        {
+        }
+
+        #region Getters & Setters
+
+        public string WordWitoutEncrytion
+        {
+            get
+            {
+                return wordWithoutEncryption;
+            }
+            set
+            {
+                this.wordWithoutEncryption = value;
+            }
+        }
+
+        public string EncryptedWord
+        {
+            get
+            {
+                return encryptedWord;
+            }
+            set
+            {
+                this.encryptedWord = value;
+            }
+        }
+
+        public string DecryptedWord
+        {
+            get
+            {
+                return decryptedWord;
+            }
+            set
+            {
+                this.decryptedWord = value;
+            }
+        }
+
+        #endregion
+
+        public string EncryptWord()
+        {
+            encryptedWord = ChangeAsciCode(wordWithoutEncryption, true);
+            return encryptedWord;
+        }
+
+        public string DecryptWord()
+        {
+            decryptedWord = ChangeAsciCode(wordWithoutEncryption, false);
+            return decryptedWord;
+        }
+
+        string ChangeAsciCode(string word, bool isEncrypt)
+        {
+            var wordArray = word.ToCharArray();
+
+            for (int i = 0; i < wordArray.Length; i++)
+            {
+                
+                var charCode = isEncrypt ? GetAsciiCodeFromChar(wordArray[i]) + 1 : GetAsciiCodeFromChar(wordArray[i]) - 1;
+                var newChar = GetCharFromAsciiCode(charCode);
+                wordArray[i] = newChar;
+            }
+
+            var resultString = new string(wordArray);
+
+            return resultString;
+        }
+
+        int GetAsciiCodeFromChar(char letter)
+        {
+            var asciiCode = (int)letter;
+            return asciiCode;
+        }
+
+        char GetCharFromAsciiCode(int code)
+        {
+            var letter = (char)code;
+            return letter;
+        }
+
+    }
+}
